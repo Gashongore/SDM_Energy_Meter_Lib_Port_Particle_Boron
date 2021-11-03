46e5f469f541e5ca2113249c8f7c3ab9c8bf167f
Prince GASHONGORE <66318914+Gashongore@users.noreply.github.com>
2021-11-04 04:50:20 +0900

    Ported SDM_Energy_Meter_Lib_Port_Particle_Boron

---
+++ target/2.2.0/boron/obj/src/newlib_stubs.o.d
@@ -0,0 +1,152 @@
+/Users/gashongore/Desktop/SDM6830/smd6830_Energy/target/2.2.0/boron//obj/./src/newlib_stubs.o: \
+ src/newlib_stubs.cpp \
+ ../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h \
+ ../../../services/inc/debug.h ../../../services/inc/service_debug.h \
+ ../../../services/inc/logging.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ ../../../services/inc/panic.h ../../../services/inc/panic_codes.h \
+ ../../../services/inc/config.h ../../../services/inc/preprocessor.h
+
+../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h:
+
+../../../services/inc/debug.h:
+
+../../../services/inc/service_debug.h:
+
+../../../services/inc/logging.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/gashongore/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+../../../services/inc/panic.h:
+
+../../../services/inc/panic_codes.h:
+
+../../../services/inc/config.h:
+
+../../../services/inc/preprocessor.h:
---
+++ target/3.1.0/boron/obj/src/user_module.o.d
@@ -0,0 +1,6 @@
+/Users/gashongore/Desktop/SDM6830/smd6830_Energy/target/3.1.0/boron//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
